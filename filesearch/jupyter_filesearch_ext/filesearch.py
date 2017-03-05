e565e1f0421533322c16aebe00398afcd1878fc5
Binh Vu <binh@toan2.com>
2017-03-05 02:35:52 -0800

    update filesearch configuration

--- filesearch/jupyter_filesearch_ext/filesearch.py
+++ filesearch/jupyter_filesearch_ext/filesearch.py
@@ -1,4 +1,4 @@
-import os, ujson
+import os, ujson, re
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 
@@ -10,7 +10,8 @@ class JupyterFileSearchExtension(IPythonHandler):
         self.search_config = {
             'ignore_extensions': { '.pyc', '.png', 'jpg', '.jpeg', '.log' },
             'ignore_names': set(),
-            'ignore_folders': set()
+            'ignore_folders': set(),
+            'ignore_regex': []
         }
 
         config_file = os.path.join(self.working_dir, 'filesearch.config.json')
@@ -20,10 +21,11 @@ class JupyterFileSearchExtension(IPythonHandler):
                 config = ujson.loads(f.read())
             self.search_config.update(config)
 
-        assert len(set(self.search_config.keys()).difference({ 'ignore_folders', 'ignore_extensions', 'ignore_names' })) == 0, 'Invalid configuration file'
+        assert len(set(self.search_config.keys()).difference({ 'ignore_folders', 'ignore_extensions', 'ignore_names', 'ignore_regex' })) == 0, 'Invalid configuration file'
         self.search_config['ignore_extensions'] = set(self.search_config['ignore_extensions'])
         self.search_config['ignore_folders'] = set([os.path.abspath(x) for x in self.search_config['ignore_folders']])
         self.search_config['ignore_names'] = set(self.search_config['ignore_names'])
+        self.search_config['ignore_regex'] = [re.compile(x) for x in self.search_config['ignore_regex']]
 
     def should_ignore_name(self, name):
         if name.startswith('.'):
@@ -41,6 +43,10 @@ class JupyterFileSearchExtension(IPythonHandler):
     def should_ignore_folder(self, dpath):
         return dpath in self.search_config['ignore_folders']
 
+    def should_ignore_regex(self, path):
+        path = path.replace(self.working_dir, '')
+        return any((r.match(path) is not None for r in self.search_config['ignore_regex']))
+
     def set_default_headers(self):
         super(JupyterFileSearchExtension, self).set_default_headers()
         self.set_header('Content-Type', 'application/json')
@@ -52,7 +58,7 @@ class JupyterFileSearchExtension(IPythonHandler):
             i = 0
             while i < len(dirnames):
                 dpath = os.path.abspath(os.path.join(dirpath, dirnames[i]))
-                if self.should_ignore_name(dirnames[i]) or self.should_ignore_folder(dpath):
+                if self.should_ignore_name(dirnames[i]) or self.should_ignore_folder(dpath) or self.should_ignore_regex(dpath):
                     del dirnames[i]
                 else:
                     files.append({
@@ -63,11 +69,12 @@ class JupyterFileSearchExtension(IPythonHandler):
 
             i = 0
             while i < len(filenames):
-                if self.should_ignore_name(filenames[i]):
+                fpath = os.path.abspath(os.path.join(dirpath, filenames[i]))
+                if self.should_ignore_name(filenames[i]) or self.should_ignore_regex(fpath):
                     del filenames[i]
                 else:
                     files.append({
-                        'path': os.path.abspath(os.path.join(dirpath, filenames[i])).replace(self.working_dir, ''),
+                        'path': fpath.replace(self.working_dir, ''),
                         'type': 'file' 
                     })
                     i += 1
